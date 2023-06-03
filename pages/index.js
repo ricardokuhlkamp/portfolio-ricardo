aabe68d0af8cd3f9117a2058d4009336fb22beea
ricardokuhlkamp <ric.kds@hotmail.com>
2023-06-03 11:19:34 -0300

    alterações na home

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,8 @@ import { AiFillLinkedin, AiOutlineFacebook, AiFillGithub, AiOutlineWhatsApp, AiF
 import Image from "next/image";
 import ricardoImg from "../public/ricardo.fundoImagemRemovido.png";
 import { FaCss3, FaHtml5, FaJs, FaReact, FaBootstrap, FaBalanceScale } from 'react-icons/fa';
-import { SiTailwindcss } from 'react-icons/si';
+import { SiTailwindcss, SiTypescript, SiDocker, SiNodedotjs } from 'react-icons/si';
+import { GrMysql } from 'react-icons/gr';
 import trybeWarts from '../public/trybeWarts.gif';
 import tryunfo from '../public/tryUnfo.gif';
 import solarSystem from '../public/solarSystem.gif';
@@ -52,9 +53,9 @@ export default function Home() {
                   onClick={ () => setDarkMode(!darkMode) }
                   className=" cursor-pointer text-2xl dark:text-gray-400" />
               </li>
-              <li>
+              {/* <li>
                 <Link className="bg-gradient-to-r from-sky-500 to-emerald-500 text-white px-4 py-2 rounded-md ml-8" href="about">Resumo</Link>
-              </li>
+              </li> */}
             </ul>
           </nav>
           <div className="text-center p-10">
@@ -113,8 +114,24 @@ export default function Home() {
             <SiTailwindcss className="text-4xl self-center text-gray-600 hover:text-[#36b7f0] transition-colors duration-200 delay-300 sm:text-8xl md:text-8xl lg:text-9xl xl:text-9xl 2xl:text-9xl" />
             <h3 className="font-medium pt-8 pb-2 text-emerald-600 sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-sm">Tailwindcss</h3>
           </div>
+          <div className="text-center flex flex-col justify-center py-3 text-gray-600 shadow-lg p-8 rounded-xl my-10 dark:bg-white dark:shadow-sky-500/50">
+            <SiTypescript className="text-4xl self-center text-gray-600 hover:text-[#3178c6] transition-colors duration-200 delay-300 sm:text-8xl md:text-8xl lg:text-9xl xl:text-9xl 2xl:text-9xl" />
+            <h3 className="font-medium pt-8 pb-2 text-emerald-600 sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-sm">Typescript</h3>
+          </div>
+          <div className="text-center flex flex-col justify-center py-3 text-gray-600 shadow-lg p-8 rounded-xl my-10 dark:bg-white dark:shadow-sky-500/50">
+            <SiDocker className="text-4xl self-center text-gray-600 hover:text-[#086dd7] transition-colors duration-200 delay-300 sm:text-8xl md:text-8xl lg:text-9xl xl:text-9xl 2xl:text-9xl" />
+            <h3 className="font-medium pt-8 pb-2 text-emerald-600 sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-sm">Docker</h3>
+          </div>
+          <div className="text-center flex flex-col justify-center py-3 text-gray-600 shadow-lg p-8 rounded-xl my-10 dark:bg-white dark:shadow-sky-500/50">
+            <SiNodedotjs className="text-4xl self-center text-gray-600 hover:text-[#578f4a] transition-colors duration-200 delay-300 sm:text-8xl md:text-8xl lg:text-9xl xl:text-9xl 2xl:text-9xl" />
+            <h3 className="font-medium pt-8 pb-2 text-emerald-600 sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-sm">Node.js</h3>
+          </div>
+          <div className="text-center flex flex-col justify-center py-3 text-gray-600 shadow-lg p-8 rounded-xl my-10 dark:bg-white dark:shadow-sky-500/50">
+            <GrMysql className="text-4xl self-center text-gray-600 hover:text-[#68a0b1] transition-colors duration-200 delay-300 sm:text-8xl md:text-8xl lg:text-9xl xl:text-9xl 2xl:text-9xl" />
+            <h3 className="font-medium pt-8 pb-2 text-emerald-600 sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-sm">MySQL</h3>
+          </div>
         </div>
-        <div className="text-center">
+        {/* <div className="text-center">
           <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">
             SoftSkills:
           </p>
@@ -136,7 +153,7 @@ export default function Home() {
             <BiBookReader className="text-4xl self-center text-gray-600 hover:text-emerald-600 sm:text-8xl md:text-8xl lg:text-9xl xl:text-9xl 2xl:text-9xl" />
             <h3 className="font-medium pt-8 pb-2 text-emerald-600 sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-sm">Disposição para aprender</h3>
           </div>
-        </div>
+        </div> */}
         <hr className="h-px my-20 bg-gray-400 border-0 dark:bg-gray-700"></hr>
         <section>
           <div className="text-center">
@@ -200,6 +217,72 @@ export default function Home() {
                 <p className="text-gray-800">HTML, CSS, ReactJS</p>
               </div>
             </div>
+          </div>
+        </section>
+        <hr className="h-px my-20 bg-gray-400 border-0 dark:bg-gray-700"></hr>
+        <section className="min-h-screen">
+        <div>
+            <h3 className="text-3xl py-1 dark:text-white">Sobre</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">Sou uma pessoa determinada, comprometida, responsável, e disposta a aprender novos conhecimentos.</p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">Possuo formação em engenharia agronômica pela UDESC-Lages/ SC e estou em transição de carreira.</p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">Fui funcionário público concursado como engenheiro agronômo/analista ambiental. Trabalhando na área ambiental, tive contato com a programação através da Linguagem-R para análise de dados de inventários florestais o que despertou novamente meu interesse pela programação e a vontande e coragem para efetuar uma transição de carreira.</p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">Estou cursando Análise e Desenvolvimento de Sistemas na UNIASSELVI. E concomitantemente sou estudante do curso de Desenvonvimento Web da Trybe!</p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">Atualmente na Trybe, como estudante de desenvolvimento web na Trybe pude aplicar as seguintes tecnologias: Git, HTML, CSS, Javascript, React, TypeScript, Docker, e MySQL.</p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">Tenho interesse em trabalhar com tecnologia, especialmente na área de programação como desenvovedor Full-Stack ou Ciência de Dados. Meus estudos são parte do movimento que faço para atingir meu objetivo de trabalhar no ramo da tecnologia!</p>
+          </div>
+
+          <hr className="h-px my-8 bg-gray-400 border-0 dark:bg-gray-700"></hr>
+
+          <div>
+            <h3 className="text-3xl pb-3 pt-4 dark:text-white">Formação Acadêmica</h3>
+
+            <h2 className="text-2xl py-1 dark:text-white">Trybe</h2>
+            <h2 className="text-xl py-1 dark:text-white">Desenvilvimento Web</h2>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">setembro de 2022 - setembro de 2023.</p>
+
+            <h2 className="text-2xl py-1 dark:text-white">UNIASSELVI</h2>
+            <h2 className="text-xl py-1 dark:text-white">Tecnólogo, Análise e Desenvolvimento de Sistemas</h2>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">outubro de 2020 - março de 2023.</p>
+
+            <h2 className="text-2xl py-1 dark:text-white">Universidade do Estado de Santa Catarina</h2>
+            <h2 className="text-xl py-1 dark:text-white">Bacharelado, Agronomia</h2>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">2004 - 2009.</p>
+          </div>
+
+          <hr className="h-px my-8 bg-gray-400 border-0 dark:bg-gray-700"></hr>
+
+          <div className="pb-20">
+            <h3 className="text-3xl pb-3 pt-4 dark:text-white">Experiência</h3>
+
+            <h2 className="text-2xl py-1 dark:text-white">Prefeitura Municipal de Balneário Barra do Sul - SC</h2>
+            <h2 className="text-xl py-1 dark:text-white">Engenheiro agrônomo</h2>
+            <h2 className="text-xl py-1 dark:text-white">março de 2019 - Present (3 anos 10 meses)</h2>
+            <h2 className="text-xl py-1 dark:text-white">Balneário Barra do Sul - SC</h2>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">Engenheiro agrônomo efetivo na Seplan (Secretaria de Planejamento), setor de Meio Ambiente, na Prefeitura Municipal de Balneário Barra do Sul - SC.</p>
+
+            <h2 className="text-2xl py-1 dark:text-white">Consultor técnico de vendas comissionado (fertilizantes Ferticel)</h2>
+            <h2 className="text-xl py-1 dark:text-white">Autônomo</h2>
+            <h2 className="text-xl py-1 dark:text-white">agosto de 2017 - março de 2019 (1 ano 8 meses)</h2>
+            <h2 className="text-xl py-1 dark:text-white">Litoral norte catarinense e grande Florianópolis</h2>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">Realização de visitas aos produtores rurais, efetuando a venda de fertilizantes orgânicos e organominerais direto de fábrica.</p>
+
+            <h2 className="text-2xl py-1 dark:text-white">Agricultura</h2>
+            <h2 className="text-xl py-1 dark:text-white">Agricultor/Engenheiro agrônomo</h2>
+            <h2 className="text-xl py-1 dark:text-white">janeiro de 2017 - agosto de 2017 (8 meses)</h2>
+            <h2 className="text-xl py-1 dark:text-white">Luis Alves, SC, Brasil</h2>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">Cultivo de hortaliças.</p>
+
+            <h2 className="text-2xl py-1 dark:text-white">Proagro agropecuária ltda me</h2>
+            <h2 className="text-xl py-1 dark:text-white">Engenheiro agrônomo</h2>
+            <h2 className="text-xl py-1 dark:text-white">janeiro de 2014 - janeiro de 2017 (3 anos 1 mês)</h2>
+            <h2 className="text-xl py-1 dark:text-white">Luis Alves, SC, Brasil</h2>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">Comercialização de insumos agrícolas.</p>
+
+            <h2 className="text-2xl py-1 dark:text-white">Credialves</h2>
+            <h2 className="text-xl py-1 dark:text-white">Engenheiro agrônomo</h2>
+            <h2 className="text-xl py-1 dark:text-white">setembro de 2009 - abril de 2013 (3 anos 8 meses)</h2>
+            <h2 className="text-xl py-1 dark:text-white">Luis Alves, SC, Brasil</h2>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">Elaboração de projetos de custeio e investimento agrícola.</p>
           </div>
         </section>
       </main>
